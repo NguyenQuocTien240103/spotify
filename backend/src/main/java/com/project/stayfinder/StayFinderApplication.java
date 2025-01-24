a7ec8571d24024e82c75036bad341488eb0dea6c
abcdebug <quocnamnehehe@gmail.com>
2025-01-24 08:48:24 +0700

    init backend

---
+++ backend/src/main/java/com/project/stayfinder/StayFinderApplication.java
@@ -0,0 +1,13 @@
+package com.project.stayfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayFinderApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/project/stayfinder/StayFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stayfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
